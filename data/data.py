e735dff48faa66e545bc095646ddb106e8ff109e
fahidrahman12 <fahid786rahman@gmail.com>
2025-12-14 17:27:17 +0000

    add data

---
+++ data/data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# URL of the CSV file (example)
+url = "https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2023-24/gws/merged_gw.csv"
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(url)
+
+df.info()
+df.to_csv(r'C:\Users\fahed\PycharmProjects\FPL\data\data.csv', index= True)
